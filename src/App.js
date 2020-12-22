72b165337e331d9431bb0c2fe402d6e1144a920f
ben mckenna <benmckenna2000@outlook.com>
2020-12-22 17:44:46 +0000

    Datalayer complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Login from "./components/Login";
+import Player from "./components/Player";
 import React, { useEffect, useState } from "react";
 import "./css/App.css";
-import "./components/Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{}, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -15,9 +20,15 @@ function App() {
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("person", user);
+      });
     }
   }, []);
-  return <div className="app">{token ? <h1>yeman</h1> : <Login />}</div>;
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/Player.css";
+
+function Player() {
+  return <div className="player"></div>;
+}
+
+export default Player;
